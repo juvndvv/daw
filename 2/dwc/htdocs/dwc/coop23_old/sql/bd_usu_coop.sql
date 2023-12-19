72032c951cd069974c0b78e5f7c05e0e6239ec4d
Juan Daniel Forner <jdanielforga@gmail.com>
2023-12-19 15:16:08 +0100

    19/12

--- 2/dwc/htdocs/dwc/coop23_old/sql/bd_usu_coop.sql
+++
@@ -1,5 +0,0 @@
-create user 'coop'@'localhost' identified by 'coop';
-grant usage on * . * to 'coop'@'localhost' identified by 'coop' ;
-create database if not exists `coop`  default character set utf8 collate utf8_spanish_ci;;
-grant all privileges on `coop` . * to 'coop'@'localhost';
-use `coop`;
